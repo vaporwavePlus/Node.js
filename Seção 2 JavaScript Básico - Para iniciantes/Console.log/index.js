df09d9827dc48ed5a091e2f63460b07fd1e750d2
João_Messias <joaomessias007@hotmail.com>
2022-09-06 01:47:36 -0300

    6 de setembro de 2022

---
+++ Seção 2 JavaScript Básico - Para iniciantes/Console.log/index.js
@@ -0,0 +1,6 @@
+console.log('Olá "João"'); //string = Texto
+console.log("Olá 'João'");
+console.log(`"Olá" 'João'`);
+
+console.log(123456); //Valor literal = Numero literal
+console.log(123456, 15.85, "João");
