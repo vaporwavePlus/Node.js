d447fd771faeec3154994e3982ed255bdae10a67
João_Messias <joaomessias007@hotmail.com>
2022-09-09 17:37:16 -0300

    Comentários de código
    index.js contem informações sobre minhas aulas dentro de curso

---
+++ Seção 2 JavaScript Básico - Para iniciantes/Comentários de código/index.js
@@ -0,0 +1,18 @@
+// Comentario
+console.log("Hello world");
+console.log("Paragrafo 1°");
+console.log("Paragrafo 2°");
+// Comentario vai ser ignorado
+console.log("Comentarios");
+// Vão ser ignorados
+
+/*
+console.log('a')
+console.log('b')
+console.log('c')
+console.log('d')
+console.log('e')
+console.log('f')
+console.log('g')
+console.log('h')
+*/
